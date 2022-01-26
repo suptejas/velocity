fddd7201fd667259e7cc5150cb43b3c662ea9987
Tejas <xtremedevx@gmail.com>
2022-01-26 23:43:40 +0400

    :fire: remove progress bar, replace with counter

--- src/net.rs
+++ src/net.rs
@@ -4,39 +4,37 @@ use indicatif::{ProgressBar, ProgressStyle};
 use owo_colors::OwoColorize;
 
 pub async fn pre_flight_network_test() {
-    let bar = ProgressBar::new(4).with_style(
+    let bar = ProgressBar::new(2).with_style(
         ProgressStyle::default_bar()
-            .template("{bar:20.cyan/black} {pos}/{len} > {msg}")
+            .template("{msg}")
             .progress_chars("██"),
     );
 
     bar.set_message(format!(
-        "{} {}",
+        "> {} {}",
         "🔗".bright_yellow(),
         "google.com".to_string().bright_green().underline()
     ));
 
-    for _ in 0..2 {
-        surf::get("https://www.google.com".to_string())
-            .recv_bytes()
-            .await
-            .unwrap_or_else(|error| {
-                bar.abandon_with_message(format!(
-                    "💥 {error} for {}",
-                    "https://www.google.com"
-                        .to_string()
-                        .bright_red()
-                        .underline()
-                ));
+    surf::get("https://www.google.com".to_string())
+        .recv_bytes()
+        .await
+        .unwrap_or_else(|error| {
+            bar.abandon_with_message(format!(
+                "💥 {error} for {}",
+                "https://www.google.com"
+                    .to_string()
+                    .bright_red()
+                    .underline()
+            ));
 
-                exit(1);
-            });
+            exit(1);
+        });
 
-        bar.inc(1);
-    }
+    bar.inc(1);
 
     bar.set_message(format!(
-        "{} {}",
+        "> {} {}",
         "🔗".bright_yellow(),
         "api.instatus.com".to_string().bright_green().underline()
     ));
